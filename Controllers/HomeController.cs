cb385c58b2421667dfd62939a1532928be9a53d8
Fabio Cataleta <fcataleta@gmail.com>
2020-04-16 17:33:00 +0100

    Added Controller and removed unnecessary usings

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloMVCWorld.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return Content("Hello, MVC World!");
+		}
+	}
+}
